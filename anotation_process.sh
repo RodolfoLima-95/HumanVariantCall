e90536c17ed5901e6daddfdf7e504ab58613a737
Rodolfo Lima <rodolfo.eli.jrles@gmail.com>
2024-09-13 20:09:01 -0300

    modificando arquivos

--- anotation_process.sh
+++ anotation_process.sh
@@ -1,44 +1,72 @@
 #!/bin/bash
 
-# Lista todos os arquivos na pasta atual
-for file in *; do
+DIR = 'data/'
+for file in "$DIR"/*; do
   # Verifica se é um arquivo (e não um diretório)
   if [ -f "$file" ]; then
     # Usa basename para remover a extensão do arquivo
     filename=$(basename "$file" | sed 's/\.[^.]*$//')
 
     gatk VariantFiltration \
-	-R reference/hg38.fa \
-	-V results/${filename}_raw_indels.vcf \
-	-O results/${filename}_filtered_indels.vcf \
-	-filter-name "QD_filter" -filter "QD < 2.0" \
-	-filter-name "FS_filter" -filter "FS > 200.0" \
-	-filter-name "SOR_filter" -filter "SOR > 10.0" \
-	-genotype-filter-expression "DP < 10" \
-	-genotype-filter-name "DP_filter" \
-	-genotype-filter-expression "GQ < 10" \
-	-genotype-filter-name "GQ_filter"
+        -R reference/hg38.fa \
+        -V results/{$filename}_raw_snps.vcf \
+        -O results/{$filename}_filtered_snps.vcf \
+        -filter-name "QD_filter" -filter "QD < 2.0" \
+        -filter-name "FS_filter" -filter "FS > 60.0" \
+        -filter-name "MQ_filter" -filter "MQ < 40.0" \
+        -filter-name "SOR_filter" -filter "SOR > 4.0" \
+        -filter-name "MQRankSum_filter" -filter "MQRankSum < -12.5" \
+        -filter-name "ReadPosRankSum_filter" -filter "ReadPosRankSum < -8.0" \
+        -genotype-filter-expression "DP < 10" \
+        -genotype-filter-name "DP_filter" \
+        -genotype-filter-expression "GQ < 10" \
+        -genotype-filter-name "GQ_filter"
 
     gatk SelectVariants \
-	--exclude-filtered \
-	-V results/${filename}_filtered_indels.vcf \
-	-O results/${filename}_analysis-ready-indels.vcf
+        --exclude-filtered \
+        -V results/{$filename}_filtered_snps.vcf \
+        -O results/{$filename}_analysis-ready-snps.vcf
 
-    cat results/${filename}_analysis-ready-snps.vcf|grep -v -E "DP_filter|GQ_filter" > results/${filename}_analysis-ready-snps-filteredGT.vcf
-    cat results/${filename}_analysis-ready-indels.vcf|grep -v -E "DP_filter|GQ_filter" > results/${filename}_analysis-ready-indels-filteredGT.vcf
+    gatk VariantFiltration \
+        -R reference/hg38.fa \
+        -V results/{$filename}_raw_indels.vcf \
+        -O results/{$filename}_filtered_indels.vcf \
+        -filter-name "QD_filter" -filter "QD < 2.0" \
+        -filter-name "FS_filter" -filter "FS > 200.0" \
+        -filter-name "SOR_filter" -filter "SOR > 10.0" \
+        -genotype-filter-expression "DP < 10" \
+        -genotype-filter-name "DP_filter" \
+        -genotype-filter-expression "GQ < 10" \
+        -genotype-filter-name "GQ_filter"
+
+    gatk SelectVariants \
+        --exclude-filtered \
+        -V results/{$filename}_filtered_indels.vcf \
+        -O results/{$filename}_analysis-ready-indels.vcf
+
+    cat results/{$filename}_analysis-ready-snps.vcf|grep -v -E "DP_filter|GQ_filter" > results/{$filename}_analysis-ready-snps-filteredGT.vcf
+    cat results/{$filename}_analysis-ready-indels.vcf|grep -v -E "DP_filter|GQ_filter" > results/{$filename}_analysis-ready-indels-filteredGT.vcf
 
     gatk Funcotator \
-    --variant results/${filename}_analysis-ready-snps-filteredGT.vcf \
-    --reference reference/hg38.fa \
-    --ref-version hg38 \
-    --data-sources-path funcotator_dataSources.v1.8.hg38.20230908g \
-    --output results/${filename}_analysis-ready-snps-filteredGT-functotated.vcf \
-    --output-file-format VCF 
- 
-    gatk Funcotator \
-        --variant results/${filename}_analysis-ready-indels-filteredGT.vcf \
+        --variant results/{$filename}_analysis-ready-snps-filteredGT.vcf \
         --reference reference/hg38.fa \
         --ref-version hg38 \
         --data-sources-path funcotator_dataSources.v1.8.hg38.20230908g \
-        --output results/${filename}_analysis-ready-indels-filteredGT-functotated.vcf \
+        --output results/{$filename}_analysis-ready-snps-filteredGT-functotated.vcf \
+        --output-file-format VCF 
+ 
+    gatk Funcotator \
+        --variant results/{$filename}_analysis-ready-indels-filteredGT.vcf \
+        --reference reference/hg38.fa \
+        --ref-version hg38 \
+        --data-sources-path funcotator_dataSources.v1.8.hg38.20230908g \
+        --output results/{$filename}_analysis-ready-indels-filteredGT-functotated.vcf \
         --output-file-format VCF
+    
+    gatk VariantsToTable \
+        -V results/{$filename}_analysis-ready-snps-filteredGT-functotated.vcf -F AC -F AN -F DP -F AF -F FUNCOTATION \
+        -O results/{$filename}_output_notations_snps.table
+
+    gatk VariantsToTable \
+	    -V results/{$filename}_analysis-ready-indels-filteredGT-functotated.vcf -F AC -F AN -F DP -F AF -F FUNCOTATION \
+	    -O results/{$filename}_output_notations_indels.table
--- variant_call_process.sh
+++ variant_call_process.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # Lista todos os arquivos na pasta atual
-for file in *; do
+DIR = 'data/'
+for file in "$DIR"/*; do
   # Verifica se é um arquivo (e não um diretório)
   if [ -f "$file" ]; then
     # Usa basename para remover a extensão do arquivo
     filename=$(basename "$file" | sed 's/\.[^.]*$//')
     
-    samtools sort -o output/${ERR050095}.align.sorted.bam output/${ERR050095}.align.sam
+    samtools sort -o output/{$filename}.align.sorted.bam output/{$filename}.bam
 
-    gatk SortSam -I output/${filename}.align.sorted.bam -O output/${filename}.align.sorted.coord.bam -SO coordinate
+    gatk SortSam -I output/{$filename}.align.sorted.bam -O output/{$filename}.align.sorted.coord.bam -SO coordinate
 
-    samtools index output/${filename}.align.sorted.coord.bam
+    samtools index output/{$filename}.align.sorted.coord.bam
 
-    gatk MarkDuplicates -I output/${filename}.align.sorted.coord.bam -O output/${filename}.align.sorted.coord.dedup.bam -M output/ERR050095.align.sorted.coord.dedup.metric.txt
+    gatk MarkDuplicates -I output/{$filename}.align.sorted.coord.bam -O output/{$filename}.align.sorted.coord.dedup.bam -M output/ERR050095.align.sorted.coord.dedup.metric.txt
 
     gatk BaseRecalibrator \
         -I output/${ERR050095}.align.sorted.coord.dedup.bam \
@@ -22,39 +23,19 @@ for file in *; do
         -O output/recal_data.table 
 
     gatk ApplyBQSR \
-        -I output/${filename}.align.sorted.coord.dedup.bam \
+        -I output/{$filename}.align.sorted.coord.dedup.bam \
         -R reference/hg38.fa \
         --bqsr-recal-file output/recal_data.table \
-        -O output/${filename}.align.sorted.coord.dedup.bqsr.bam
+        -O output/{$filename}.align.sorted.coord.dedup.bqsr.bam
 
     gatk HaplotypeCaller \
         -R reference/hg38.fa \
-        -I output/${filename}.align.sorted.coord.dedup.bqsr.bam \ 
+        -I output/{$filename}.align.sorted.coord.dedup.bqsr.bam \ 
         -ERC GVCF \ 
         -L chr20 \
-        -O results/${filename}_raw_variants.vcf
+        -O results/{$filename}_raw_variants.vcf
     
-    bcftools view -v snps results/${filename}_raw_variants.vcf -o results/${filename}_raw_snps.vcf
+    bcftools view -v snps results/{$filename}_raw_variants.vcf -o results/{$filename}_raw_snps.vcf
 
-    bcftools view -v indels results/${filename}_raw_variants.vcf -o results/${filename}_raw_indels.vcf
-
-    gatk VariantFiltration \
-	-R reference/hg38.fa \
-	-V results/${filename}_raw_snps.vcf \
-	-O results/${filename}_filtered_snps.vcf \
-	-filter-name "QD_filter" -filter "QD < 2.0" \
-	-filter-name "FS_filter" -filter "FS > 60.0" \
-	-filter-name "MQ_filter" -filter "MQ < 40.0" \
-	-filter-name "SOR_filter" -filter "SOR > 4.0" \
-	-filter-name "MQRankSum_filter" -filter "MQRankSum < -12.5" \
-	-filter-name "ReadPosRankSum_filter" -filter "ReadPosRankSum < -8.0" \
-	-genotype-filter-expression "DP < 10" \
-	-genotype-filter-name "DP_filter" \
-	-genotype-filter-expression "GQ < 10" \
-	-genotype-filter-name "GQ_filter"
-
-    gatk SelectVariants \
-	--exclude-filtered \
-	-V results/${filename}_filtered_snps.vcf \
-	-O results/${filename}_analysis-ready-snps.vcf
+    bcftools view -v indels results/{$filename}_raw_variants.vcf -o results/{$filename}_raw_indels.vcf
 
