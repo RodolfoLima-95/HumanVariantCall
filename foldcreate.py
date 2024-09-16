76d42bea103d391e5a1cc9d6d72b3da4ee99d588
Rodolfo Lima <rodolfo.eli.jrles@gmail.com>
2024-09-16 10:05:19 -0300

    correções

--- foldcreate.py
+++ foldcreate.py
@@ -5,8 +5,10 @@ Editor Spyder
 Este é um arquivo de script temporário.
 """
 
+folders = ['data','results','scripts','output','tools' ]
 import os
 import argparse
+import shutil
 
 def create_directory(folder_name):
     # Verifica se o diretório já existe
@@ -16,6 +18,43 @@ def create_directory(folder_name):
         print(f'Pasta "{folder_name}" criada com sucesso!')
     else:
         print(f'Pasta "{folder_name}" já existe.')
+
+    for i in folders:
+        subfolder_path = os.path.join(folder_name, i)
+        if not os.path.exists(subfolder_path):
+            os.makedirs(subfolder_path)
+            print(f'Subpasta "{i}" criada com sucesso dentro de "{folder_name}"!')
+        else:
+            print(f'Subpasta "{i}" já existe dentro de "{folder_name}".')
+
+
+
+    def copiar_pasta():
+        origem="/home/jrodolfo/Documents/projetos/SRR28738518"
+        destino=f"/home/jrodolfo/Documents/projetos/{folder_name}/reference/"
+        # Verifica se o diretório de origem existe
+        if not os.path.exists(origem):
+            print(f"A pasta de origem '{origem}' não existe.")
+            return
+
+        # Verifica se o destino já existe
+#        if os.path.exists(destino):
+#            print(f"O destino '{destino}' já existe. Escolha outro destino ou remova a pasta existente.")
+#            return
+
+        try:
+            # Copia a pasta de origem para o destino
+            shutil.copytree(origem, destino)
+            print(f"A pasta '{origem}' foi copiada com sucesso para '{destino}'.")
+        except Exception as e:
+            print(f"Erro ao copiar a pasta: {e}")
+
+    if __name__ == "__main__":
+
+
+        # Chama a função para copiar a pasta
+        copiar_pasta()
+
 
 if __name__ == "__main__":
     # Define o parser de argumentos
